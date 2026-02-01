b92143a79e7c6e896a7daebb8937a764b46be2f7
davidcoral2-cell <gtbyy2@gmail.com>
2026-02-01 20:46:18 +0100

    Proyecto_Finalizado_v2

--- js/firebase.js
+++ js/firebase.js
@@ -2,16 +2,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
-
-const firebaseConfig = {
-    apiKey: "AIzaSyB9G3Iwg7amAwpCZXwGsTVxOP5TJppH9DI",
-    authDomain: "skinhub-40408.firebaseapp.com",
-    projectId: "skinhub-40408",
-    storageBucket: "skinhub-40408.appspot.com",
-    messagingSenderId: "380720923974",
-    appId: "1:380720923974:web:e3de93b36b68d7e1575147",
-    measurementId: "G-7YP7Q61FVC"
-};
+import { firebaseConfig } from "./config.js";
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
